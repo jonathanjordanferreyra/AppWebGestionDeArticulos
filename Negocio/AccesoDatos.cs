c8b89feae26fbb0391f8ab565a39fbbdd524fe88
Jonathan <jonathanjordanf3@gmail.com>
2025-12-14 18:21:09 -0300

    Agrego clases acceso a datos y articulo negocio, y metodo listar articulo

---
+++ Negocio/AccesoDatos.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class AccesoDatos
+    {
+        private SqlConnection Conexion;
+        private SqlCommand Comando;
+        private SqlDataReader lector;
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public AccesoDatos()
+        {
+            Conexion = new SqlConnection("server=.\\SQLEXPRESS; database=CATALOGO_WEB_DB; integrate security=true");
+            Comando = new SqlCommand();
+        }
+        public void SetearConsulta(string consulta)
+        {
+            Comando.CommandType = System.Data.CommandType.Text;
+            Comando.CommandText = consulta;
+        }
+        public void EjecutarLectura()
+        {
+            Comando.Connection = Conexion;
+            try
+            {
+                Conexion.Open();
+                lector = Comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public void SetearParametros(string nombre, object valor)
+        {
+            Comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+        public void EjecutarAccion()
+        {
+            try
+            {
+                Comando.Connection = Conexion;
+                Conexion.Open();
+                Comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                //Limpio los parametros de anteriores consultas
+                Comando.Parameters.Clear();
+                CerrarConexion();
+            }
+        }
+        public void CerrarConexion()
+        {
+            if (lector != null)
+            {
+                lector.Close();
+            }
+            Conexion.Close();
+        }
+    }
+}
---
+++ Negocio/ArticuloNegocio.cs
@@ -0,0 +1,81 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class ArticuloNegocio
+    {
+        public List<Articulo> ListarArticulos()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.SetearConsulta("Select A.Id,A.Nombre,A.Codigo,A.Descripcion,A.ImagenUrl,A.Precio,A.IdMarca,A.IdCategoria,M.Descripcion as Marca,C.Descripcion as Categoria from ARTICULOS as A,MARCAS as M,CATEGORIAS as C where A.IdCategoria = C.Id and A.IdMarca = M.Id");
+                datos.EjecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    Articulo articulo = new Articulo();
+                    //Valido si no es null, conforme si la columna admite un DBNULL.
+                    articulo.Id = (int)datos.Lector["Id"];
+                    if (datos.Lector["Nombre"] != DBNull.Value)
+                    {
+                        articulo.Nombre = (string)datos.Lector["Nombre"];
+                    }
+                    if (datos.Lector["Codigo"] != DBNull.Value)
+                    {
+                        articulo.Codigo = (string)datos.Lector["Codigo"];
+                    }
+                    if (datos.Lector["Descripcion"] != DBNull.Value)
+                    {
+                        articulo.Descripcion = (string)datos.Lector["Descripcion"];
+                    }
+                    if (datos.Lector["ImagenUrl"] != DBNull.Value)
+                    {
+                        articulo.ImagenUrl = (string)datos.Lector["ImagenUrl"];
+                    }
+                    if (datos.Lector["Precio"] != DBNull.Value)
+                    {
+                        articulo.Precio = (decimal)datos.Lector["Precio"];
+                    }
+
+                    articulo.Marca = new Marca();
+                    if (datos.Lector["IdMarca"] != DBNull.Value)
+                    {
+                        articulo.Marca.Id = (int)datos.Lector["IdMarca"];
+                    }
+                    if (datos.Lector["Marca"] != DBNull.Value)
+                    {
+                        articulo.Marca.Descripcion = (string)datos.Lector["Marca"];
+                    }
+                    articulo.Categoria = new Categoria();
+                    if (datos.Lector["IdCategoria"] != DBNull.Value)
+                    {
+                        articulo.Categoria.Id = (int)datos.Lector["IdCategoria"];
+                    }
+                    if (datos.Lector["Categoria"] != DBNull.Value)
+                    {
+                        articulo.Categoria.Descripcion = (string)datos.Lector["Categoria"];
+                    }
+
+                    lista.Add(articulo);
+
+                }
+
+                return lista;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+    }
+}
